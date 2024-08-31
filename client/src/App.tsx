d47596270d1dd93d543b373d38bc541d50c05f99
VuFeg <ngduogtruongvu@gmail.com>
2024-08-31 13:42:54 +0700

    update authentication ui for fe

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,31 @@
+import { Route, Routes } from "react-router-dom";
+import { LogInPage } from "./pages/login/LogInPage";
+import { HomePage } from "./pages/home/HomePage";
+import { RegisterPage } from "./pages/register/RegisterPage";
+import { AuthLayout } from "./layouts/authLayout/AuthLayout";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/login"
+          element={
+            <AuthLayout>
+              <LogInPage />
+            </AuthLayout>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <AuthLayout>
+              <RegisterPage />
+            </AuthLayout>
+          }
+        />
+      </Routes>
+    </>
+  );
 }
