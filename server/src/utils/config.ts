73dc5eb2f6ff75422600fc61f6ceaf238b57e978
Tyn <tuyenpham1104.dev@gmail.com>
2024-09-09 16:51:05 +0700

    chore: update config

--- server/src/utils/config.ts
+++ server/src/utils/config.ts
@@ -8,6 +8,8 @@ export const envConfig = {
   mongodbName: process.env.MONGODB_DB_NAME as string,
   mongodbUsersCollection: process.env.MONGODB_USERS_COLLECTION as string,
   mongodbRefreshTokenCollection: process.env.MONGODB_REFRESH_TOKEN_COLLECTION as string,
+  mongodbFollowersCollection: process.env.MONGODB_FOLLOWERS_COLLECTION as string,
+  mongodbPostsCollection: process.env.MONGODB_POSTS_COLLECTION as string,
 
   accessTokenSecret: process.env.ACCESS_TOKEN_SECRET as string,
   refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET as string
