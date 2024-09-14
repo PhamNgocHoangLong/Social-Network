dc2841e38fa4f2305d3bc63580ed9f62028f0dd8
Tyn <tuyenpham1104.dev@gmail.com>
2024-09-14 13:50:05 +0700

    feat: create api medias

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,15 +5,21 @@ import { defaultErrorHandler } from './middlewares/error.middleware'
 import database from './services/database.services'
 import authRouter from './routes/auth.routes'
 import postRouter from './routes/post.routes'
+import { initFolder } from './utils/files'
+import { UPLOAD_IMAGE_TEMP_DIR } from './constants/dir'
+import mediaRouter from './routes/media.routes'
 
 const app = express()
 const PORT = envConfig.port
+
+initFolder(UPLOAD_IMAGE_TEMP_DIR)
 
 app.use(cors())
 app.use(express.json())
 
 app.use('/auth', authRouter)
 app.use('/posts', postRouter)
+app.use('/medias', mediaRouter)
 
 app.use(defaultErrorHandler)
 
